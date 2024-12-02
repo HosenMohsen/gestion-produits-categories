f93cc5de145bed15ca5efc62dfda14ffb47eb6e8
Mohsen Hosen <mohsen.hosen95@gmail.fr>
2024-12-03 00:21:49 +0100

    Initialisation React avec vite

---
+++ frontend_react/src/App.jsx
@@ -0,0 +1,10 @@
+import './output.css';
+
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-white text-4xl font-bold">Hello Tailwind CSS!</h1>
+    </div>
+  );
+}
